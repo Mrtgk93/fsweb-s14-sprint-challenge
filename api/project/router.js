358d3578df62096b4cf5cc539007c0b17022b269
Mrtgk93 <mrtgk93@hotmail.com>
2023-04-01 19:57:40 +0300

    ödev tamamlandı

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,23 @@
 //  `/api/projects` router buraya
+const router = require("express").Router();
+const projectModel = require("./model");
+
+router.get("/", async (req, res, next) => {
+  try {
+    let allProjects = await projectModel.getAllProjects();
+    res.json(allProjects);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    let inserted = await projectModel.createProject(req.body);
+    res.json(inserted);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Server'ı buradan başlatın
+const server = require("./api/server");
+
+const port = 9001;
+
+server.listen(port, () => {
+  console.log("listenin on port ", port);
+});
